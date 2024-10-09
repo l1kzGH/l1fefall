47daddc3843cfc357f3106d3fe52a3c1c311ed1f
l1kzGH <lezasw@gmail.com>
2024-10-09 23:36:03 +0400

    update camera-capture and so movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
 
-    public Transform target; // Main-person
-    public float height = 2f; // above
-    public float distance = 3f; // from
+    public Transform playerTransform; // Main-person
+    public float mouseSensitivity = 500f;
+    private float height = 2f; // above
+    private float distance = 3f; // from
+    private float xRotation = 0f; // Вращение по оси X
 
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; // cursor turns OFF
+        //Cursor.lockState = CursorLockMode.None; // to unlock
     }
 
     void Update()
     {
-        Vector3 desiredPosition = target.position + new Vector3(0, height, -distance);
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Debug.Log($"Mouse X: {mouseX}, Mouse Y: {mouseY}");
+
+        // Y
+        playerTransform.Rotate(Vector3.up * mouseX);
+
+        // xRotation update
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -15f, 45f); // lock
+
+        // Fix-pos
+        Quaternion rotation = Quaternion.Euler(xRotation, playerTransform.eulerAngles.y, 0); // Угол поворота камеры
+        Vector3 desiredPosition = playerTransform.position - rotation * Vector3.forward * distance + Vector3.up * height;
+
         transform.position = desiredPosition;
-        transform.LookAt(target);
+
+        transform.LookAt(playerTransform.position + Vector3.up * height);
     }
+
+    // void LateUpdate()
+    // {
+    //     Vector3 desiredPosition = playerTransform.position + new Vector3(0, height, -distance);
+    //     transform.position = desiredPosition;
+    //     transform.LookAt(playerTransform);
+    // }
 }
--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -8,12 +8,14 @@ public class PlayerMov : MonoBehaviour
     public float speed = 5f;
     public float jumpPower = 5f;
     private Rigidbody rb;
+    private Camera camera;
     private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        camera = Camera.main;
         isGrounded = true;
     }
 
@@ -23,10 +25,22 @@ public class PlayerMov : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized * speed;
+        Vector3 cameraForward = camera.transform.forward; // forward
+        Vector3 cameraRight = camera.transform.right; // right
 
+        // remove Y-const
+        cameraForward.y = 0; 
+        cameraRight.y = 0;
+
+        // normalizin
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 movement = (cameraForward * moveVertical + cameraRight * moveHorizontal).normalized * speed;
         rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+    }
 
+    private void Update() {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             Jump();
@@ -50,9 +64,9 @@ public class PlayerMov : MonoBehaviour
             Debug.Log("Contact with ground: " + collidedObjectName);
             isGrounded = true;
         }
-        else
-        {
-            Debug.Log("Contact with ?: " + collidedObjectName);
-        }
+        // else
+        // {
+        //     Debug.Log("Contact with ?: " + collidedObjectName);
+        // }
     }
 }
